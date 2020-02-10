b07b1b841d583bfff6fc7c187fce971139ce66ee
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-02-10 18:01:19 +0900

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import axios from "axios";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    onTermSubmit("buildings");
+  }, []);
+
+  const KEY = "AIzaSyBB1VjA1Qz31EN2S0cj9e-nH-ob_Lg-dcA";
+
+  const onTermSubmit = async term => {
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          q: term,
+          part: "snippet",
+          maxResults: 5,
+          key: KEY
+        }
+      }
+    );
+
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
+  };
+
+  const onVideoSelect = video => {
+    setSelectedVideo(video);
+  };
+
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
