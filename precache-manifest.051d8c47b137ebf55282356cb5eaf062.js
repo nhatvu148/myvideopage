6ac4b2917c36a1e0e584a88e0dd2e768819209fc
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-02-10 17:40:59 +0900

    Updates

---
+++ precache-manifest.051d8c47b137ebf55282356cb5eaf062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eded1e042bd99a654a34a193b50cb2ea",
+    "url": "/myvideopage/index.html"
+  },
+  {
+    "revision": "40bbf680ec9bd72d45e5",
+    "url": "/myvideopage/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "c2f361bd02044600814e",
+    "url": "/myvideopage/static/js/2.a6b2a003.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myvideopage/static/js/2.a6b2a003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40bbf680ec9bd72d45e5",
+    "url": "/myvideopage/static/js/main.814d1878.chunk.js"
+  },
+  {
+    "revision": "7c76b88d38b8ec40c192",
+    "url": "/myvideopage/static/js/runtime-main.1d64bc27.js"
+  }
+]);
